621626e14955ed8198191b93c9137a7aeb7192b1
aa <aa@a.a>
2024-05-26 03:27:48 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+
+function MyBlock({ size, children }) {
+  let [num, setNum] = useState(1)
+  let [label, setLabel] = useState(10)
+  const handleClick = () => {
+    // alert(`clicked ${size}`)
+    // setNum(++num)
+    ++label
+    setNum(++num)
+    if (num % 10 == 0) {
+      setLabel(++label)
+    }
+  }
+
+  useEffect(() => {
+    console.log('mounted')
+    return () => {
+      console.log('退出')
+    }
+  }, [])
+  useEffect(() => {
+    console.log('num改变')
+    return () => {
+      console.log('退出2')
+    }
+  }, [num])
+
+  return (
+    <>
+      <div style={{ color: 'red', fontSize: size || '22px' }}>
+        hi , boy - {num}
+        <p>{'label:' + label}</p>
+
+      </div>
+      <p onClick={handleClick}>{children}</p>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* {MyBlock({})} */}
+        <MyBlock size='30px'></MyBlock>
+        <MyBlock size='30px'>
+          <button>点击</button>
+        </MyBlock>
       </header>
     </div>
   );
