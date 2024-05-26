6659f8421c29bad1060a0744951f1cec6baf120b
aa <aa@a.a>
2024-05-27 02:52:41 +0800

    save

--- src/about/TeamPage.tsx
+++ src/about/TeamPage.tsx
@@ -6,14 +6,19 @@ function TeamPage() {
     const { id } = useParams()
     const [query, setQuery] = useSearchParams()
 
-    const { state } = useLocation()
+    // const { state } = useLocation()
+    // 两次结构的写法
+    const { state: { a, fromHome } } = useLocation()
 
 
 
     return <div>TeamPage
         <div>id:{id}</div>
         <div>age:{query.get('age')}</div>
-        <div>state:{state?.a}</div>
+        <div>state:{a}</div>
+        <div>state:{fromHome}</div>
+        {/* true显示为空 */}
+        <div>state:{fromHome ? '对' : '错'}</div>
     </div>
 }
 
