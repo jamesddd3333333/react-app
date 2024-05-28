e4c789c178ea2ace96cb51fe69b0c70d04f5b834
aa <aa@a.a>
2024-05-28 15:34:22 +0800

    为了使App组件能够访问Context，MyContext.Provider必须在组件树中高于App的位置

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 // App.tsx
-import React from 'react';
-import ContextProvider from './ContextProvider';
-import ConsumerComponent from './ConsumerComponent';
+import React, { useContext } from 'react';
+import MyContext, { } from './MyContext';
+import { MyContextType } from './types';
 
 const App: React.FC = () => {
+  // 使用useContext Hook来访问Context
+  const { value, setValue } = useContext<MyContextType>(MyContext);
+
   return (
-    <ContextProvider>
-      <ConsumerComponent />
-    </ContextProvider>
+    <div>
+      <h1>App Component</h1>
+      <p>Context Value: {value}</p>
+      <button onClick={() => setValue('Updated Value')}>Update Context</button>
+    </div>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import ContextProvider from './ContextProvider';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+
   </React.StrictMode>
 );
 
