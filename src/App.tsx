2bb6a22024a2dd9c1073cdcb3bf93fd6c874f2bc
aa <aa@a.a>
2024-05-29 15:42:52 +0800

    ts有报错

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 
+
+
 function App() {
+  // Parameter 'state' implicitly has an 'any' type. Parameter 'action' implicitly has an 'any' type.
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'add':
+        return { count: state.count++ };
+      case 'minus':
+        return { count: state.count-- };
+      default:
+        throw new Error()
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+  const add = () => {
+    dispatch({ type: 'add' })
+  }
+  const minus = () => {
+    dispatch({ type: 'minus' })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={add}>加</button>
+      <button onClick={minus}>减</button>
     </div>
   );
 }
